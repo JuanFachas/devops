fdfb3ced14dfc883fd85931f03df118aa939ab84
JuanFachas <167483854+JuanFachas@users.noreply.github.com>
2025-07-28 18:24:50 -0500

    Update lb_analisissonarqube.groovy

--- src/org/devops/lb_analisissonarqube.groovy
+++ src/org/devops/lb_analisissonarqube.groovy
@@ -1,25 +1,24 @@
 package org.devops
 
-
-def testCoverage(){
-    sh 'npm test' 
+def testCoverage() {
+    sh 'npm test'
 }
 
-def analisisSonar(gitName){
+def analisisSonar(gitName) {
     def scannerHome = tool 'sonar-scanner'
-    if(scannerHome){
-        withSonarQubeEnv('sonar-scanner'){
-            sh "${scannerHome}/bin/sonar-scanner \
-            -Dsonar.projectKey=react-test-jenkinsfile \
-            -Dsonar.projectName=React Test Jenkinsfile \
-            -Dsonar.sources=${env.source} \
-            -Dsonar.tests=src/__test__ \
-            -Dsonar.exclusions='**/*.test.js' \
-            -Dsonar.testExecutionReportPaths=./test-report.xml \
-            -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info"
-            
+    if (scannerHome) {
+        withSonarQubeEnv('sonar-scanner') {
+            sh """${scannerHome}/bin/sonar-scanner \
+  -Dsonar.projectKey=react-test-jenkinsfile \
+  -Dsonar.projectName='React Test Jenkinsfile' \
+  -Dsonar.sources=src \
+  -Dsonar.tests=src/__test__ \
+  -Dsonar.exclusions=**/*.test.js \
+  -Dsonar.testExecutionReportPaths=./test-report.xml \
+  -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info
+            """
         }
-    } else{
+    } else {
         error 'SonarQube Scanner not found'
     }
 }
