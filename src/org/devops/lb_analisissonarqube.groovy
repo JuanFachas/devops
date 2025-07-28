a488c280827d926e49af24e08be82a79ab270b65
JuanFachas <167483854+JuanFachas@users.noreply.github.com>
2025-07-28 18:09:40 -0500

    Update lb_analisissonarqube.groovy

--- src/org/devops/lb_analisissonarqube.groovy
+++ src/org/devops/lb_analisissonarqube.groovy
@@ -9,15 +9,16 @@ def analisisSonar(gitName){
     def scannerHome = tool 'sonar-scanner'
     if(scannerHome){
         withSonarQubeEnv('sonar-scanner'){
-            sh "${scannerHome}/bin/sonar-scanner \
+            sh """${scannerHome}/bin/sonar-scanner \
             -Dsonar.projectKey=react-test-jenkinsfile \
-            -Dsonar.projectName=React Test Jenkinsfile} \
-            -Dsonar.sources=${env.source} \
+            -Dsonar.projectName=React Test Jenkinsfile \
+            -Dsonar.sources=src \
             -Dsonar.tests=src/__test__ \
-            -Dsonar.exclusions='**/*.test.js' \
+            -Dsonar.exclusions=**/*.test.js \
             -Dsonar.testExecutionReportPaths=./test-report.xml \
-            -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info"
-            
+            -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info
+            """
+
         }
     } else{
         error 'SonarQube Scanner not found'
