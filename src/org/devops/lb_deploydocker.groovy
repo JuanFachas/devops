73da5adf719582d4f0358b83de3210d13146f9e5
JuanFachas <juanes.orteguita.2006@gmail.com>
2025-07-28 20:48:57 -0500

    Update

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,8 +1,8 @@
 package org.devops
 
 def despliegueContenedor(projectGitName){
-    //sh "docker stop ${projectGitName}"
-    //sh "docker rm ${projectGitName}"
+    sh "docker stop ${projectGitName}"
+    sh "docker rm ${projectGitName}"
     sh "docker pull juandevop/react-test-jenkinsfile"
     sh """ docker run -d --name ${projectGitName} \
     --network=${env.NameNetwork} -p 5174:5174 \
